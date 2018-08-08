20321e6c50446d991a184ae86268bbbddf4ecb80
fbio1 <fbiohenrique1@gmail.com>
2018-08-08 15:31:25 -0300

    Ultimo commit

--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
@@ -1,18 +1,20 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.EditText;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class TesteActivity extends AppCompatActivity {
 
-    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,38 +22,57 @@ public class TesteActivity extends AppCompatActivity {
 
         Button cancelar = findViewById(R.id.cancelar);
         Button ok = findViewById(R.id.ok);
-
-        EditText digita = findViewById(R.id.digite);
-
         ImageView imagem = findViewById(R.id.imagem);
 
         cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED, intent);
                 finish();
             }
         });
 
+
+        Bundle test = getIntent().getExtras();
+        final int valor = test.getInt("botao");
+
+        if(valor == 1){
+            Log.i("meuapp","Identificou");
+            imagem.setImageResource(R.drawable.teste1);
+        }
+        else if(valor == 2){
+            Log.i("meuapp","Identificou");
+            imagem.setImageResource(R.drawable.teste2);
+        }
+        else{
+            Log.i("meuapp","Identificou");
+            imagem.setImageResource(R.drawable.teste3);
+        }
+
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
+                EditText digita = findViewById(R.id.digite);
+                if (digita.getText().toString().equals("")){
+                    Toast.makeText(TesteActivity.this, "Campo vazio", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intent = new Intent();
+                    Bundle data = new Bundle();
+
+                    data.putString("resultado", digita.getText().toString());
+                    intent.putExtras(data);
+
+                    setResult(valor, intent);
+
+                    finish();
+                }
             }
         });
 
-        Bundle test = getIntent().getExtras();
-
-        if(test.getInt("botao") == 1){
-            //Log.i("meuapp","Identificou");
-            imagem.setImageResource(R.id.teste1);
-        }
-        else if(test.getInt("botao") == 2){
-            //Log.i("meuapp","Identificou");
-            imagem.setImageDrawable(getResources().getDrawable(R.id.teste2));
-        }
-        else{
-            //Log.i("meuapp","Identificou");
-            imagem.setImageDrawable(getResources().getDrawable(R.id.teste3));
-        }
     }
+
+
 }
